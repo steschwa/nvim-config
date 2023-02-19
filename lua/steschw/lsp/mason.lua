d039c89e226cd9cb51868809b3a3dc41618b41bf
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2023-02-19 19:46:38 +0100

    chore: use lua_ls to replace deprecated

--- lua/steschw/lsp/mason.lua
+++ lua/steschw/lsp/mason.lua
@@ -1,7 +1,4 @@
-local status_ok, mason = pcall(require, "mason")
-if not status_ok then
-    return
-end
+local mason = require("mason")
 
 mason.setup({
     ui = {
@@ -13,11 +10,8 @@ mason.setup({
     }
 })
 
-local mason_lspconfig_status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not mason_lspconfig_status_ok then
-    return
-end
+local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({
-    ensure_installed = { "sumneko_lua", "tsserver", "jsonls", "cssls" }
+    ensure_installed = { "lua_ls", "tsserver", "jsonls", "cssls" }
 })
--- lua/steschw/lsp/servers.lua
+++ lua/steschw/lsp/servers.lua
@@ -1,25 +1,20 @@
-local status_ok, lspconfig = pcall(require, "lspconfig")
-if not status_ok then
-    return
-end
+local lspconfig = require("lspconfig")
 
 local options = {
     on_attach = require("steschw.lsp.handlers").on_attach,
     capabilities = require("steschw.lsp.handlers").capabilities,
 }
 
-if lspconfig.sumneko_lua ~= nil then
-    local sumneko_opts = require("steschw.lsp.settings.sumneko_lua")
-    lspconfig.sumneko_lua.setup(vim.tbl_deep_extend("force", sumneko_opts, options))
+if lspconfig.lua_ls ~= nil then
+    local lua_opts = require("steschw.lsp.settings.lua_ls")
+    lspconfig.lua_ls.setup(vim.tbl_deep_extend("force", lua_opts, options))
 end
 
 if lspconfig.tsserver ~= nil then
-    local ts_server_status_ok, ts_server = pcall(require, "typescript")
-    if ts_server_status_ok then
-        ts_server.setup({
-            server = options,
-        })
-    end
+    local ts_server = require("typescript")
+    ts_server.setup({
+        server = options,
+    })
 end
 
 if lspconfig.cssls ~= nil then
@@ -40,14 +35,6 @@ if lspconfig.jsonls ~= nil then
     lspconfig.jsonls.setup(vim.tbl_deep_extend("force", jsonls_opts, options))
 end
 
-if lspconfig.pyright ~= nil then
-    lspconfig.pyright.setup(options)
-end
-
 if lspconfig.prismals ~= nil then
     lspconfig.prismals.setup(options)
-end
-
-if lspconfig.dartls ~= nil then
-    lspconfig.dartls.setup(options)
 end
