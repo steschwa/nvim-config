0b54a4d0c37459dad937eda13434075c234b7069
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2023-07-25 18:31:52 +0200

    chore: update dependencies

--- lua/steschw/plugins/lsp.lua
+++ lua/steschw/plugins/lsp.lua
@@ -134,7 +134,6 @@ return {
     },
     {
         "williamboman/mason.nvim",
-        build = ":MasonUpdate",
         cmd = "Mason",
         config = function()
             require("mason").setup()
