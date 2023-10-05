623959973c6eed17560c481db1057fdf755c3311
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2023-10-05 17:28:24 +0200

    feat: add hurl and *.Dockerfile icons

--- lua/steschw/plugins/core.lua
+++ lua/steschw/plugins/core.lua
@@ -1,5 +1,26 @@
 return {
     { "folke/lazy.nvim", version = "*" },
     { "nvim-lua/plenary.nvim", lazy = true },
-    { "nvim-tree/nvim-web-devicons", lazy = true },
+    {
+        "nvim-tree/nvim-web-devicons",
+        lazy = true,
+        opts = {
+            override_by_extension = {
+                -- https://github.com/nvim-tree/nvim-web-devicons/blob/master/lua/nvim-web-devicons.lua
+                ["Dockerfile"] = {
+                    icon = "󰡨",
+                    color = "#458ee6",
+                    cterm_color = "68",
+                    name = "Dockerfile",
+                },
+                ["hurl"] = {
+                    icon = "",
+                    color = "#ff0288",
+                    cterm_color = "198",
+                    name = "Hurl",
+                },
+            },
+        },
+        config = true,
+    },
 }
