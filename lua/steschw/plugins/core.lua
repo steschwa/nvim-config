af919e193cc646942e3128578809a1941bccc4e6
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2023-10-08 22:28:02 +0200

    refactor: use updated icons

--- lua/steschw/plugins/core.lua
+++ lua/steschw/plugins/core.lua
@@ -4,23 +4,5 @@ return {
     {
         "nvim-tree/nvim-web-devicons",
         lazy = true,
-        opts = {
-            override_by_extension = {
-                -- https://github.com/nvim-tree/nvim-web-devicons/blob/master/lua/nvim-web-devicons.lua
-                ["Dockerfile"] = {
-                    icon = "󰡨",
-                    color = "#458ee6",
-                    cterm_color = "68",
-                    name = "Dockerfile",
-                },
-                ["hurl"] = {
-                    icon = "",
-                    color = "#ff0288",
-                    cterm_color = "198",
-                    name = "Hurl",
-                },
-            },
-        },
-        config = true,
     },
 }
