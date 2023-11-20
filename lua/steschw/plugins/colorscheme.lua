04094e6e57b4e64f449f66309c2321e54446ed8b
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2023-11-20 20:30:57 +0100

    feat: more hl customization

--- lua/steschw/plugins/colorscheme.lua
+++ lua/steschw/plugins/colorscheme.lua
@@ -12,28 +12,33 @@ return {
                 on_highlights = function(highlights, colors)
                     local origin_shade = "#2B303B"
 
-                    highlights["TelescopeResultsNormal"] = { bg = origin_shade }
-                    highlights["TelescopeResultsBorder"] = { bg = origin_shade, fg = origin_shade }
-                    highlights["TelescopePreviewNormal"] = { bg = origin_shade }
-                    highlights["TelescopePreviewBorder"] = { bg = origin_shade, fg = origin_shade }
+                    local selection = { bg = colors.polar_night.bright }
+                    local float_border = { bg = origin_shade, fg = origin_shade }
+                    local float_normal = { bg = origin_shade, fg = colors.snow_storm.origin }
+
+                    highlights["TelescopeBorder"] = float_border
+                    highlights["TelescopeNormal"] = float_normal
                     highlights["TelescopePromptNormal"] = { bg = colors.polar_night.bright }
                     highlights["TelescopePromptBorder"] = {
                         bg = colors.polar_night.bright,
                         fg = colors.polar_night.bright,
                     }
                     highlights["TelescopePromptTitle"] = { fg = colors.frost.artic_water }
-                    highlights["TelescopeSelection"] = { bg = colors.polar_night.bright }
-                    highlights["Title"] = { fg = colors.frost.artic_water, bg = origin_shade }
+                    highlights["TelescopeSelection"] = selection
+                    highlights["Title"] =
+                        { fg = colors.frost.artic_water, bg = origin_shade, bold = true }
                     highlights["TelescopeTitle"] = { link = "Title" }
                     highlights["TelescopeMultiIcon"] = { fg = colors.aurora.yellow }
                     highlights["TelescopeMultiSelection"] = { fg = colors.aurora.yellow }
 
-                    highlights["CmpNormal"] = { bg = origin_shade, fg = colors.snow_storm.origin }
-                    highlights["CmpBorder"] = { bg = origin_shade, fg = origin_shade }
-                    highlights["CmpCursorLine"] = { bg = colors.polar_night.bright }
+                    highlights["CmpNormal"] = float_normal
+                    highlights["CmpBorder"] = float_border
+                    highlights["CmpCursorLine"] = selection
 
-                    highlights["FloatBorder"] = { bg = origin_shade, fg = origin_shade }
-                    highlights["NormalFloat"] = { bg = origin_shade }
+                    highlights["FloatBorder"] = float_border
+                    highlights["NormalFloat"] = float_normal
+                    highlights["Pmenu"] = { bg = origin_shade }
+                    highlights["PmenuSel"] = selection
                 end,
             })
             vim.cmd.colorscheme("nord")
