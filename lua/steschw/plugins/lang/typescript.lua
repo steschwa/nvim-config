a0e163f9f82af4c6a1b0900530cb5e87453bba92
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-02-02 15:24:52 +0100

    refactor: set lsp related keymaps in on_attach

--- lua/steschw/plugins/lang/typescript.lua
+++ lua/steschw/plugins/lang/typescript.lua
@@ -1,16 +1,22 @@
 local deps = require("steschw.utils.dependencies")
+local keymap = require("steschw.utils.keys").keymap
 
 return {
     {
         "pmizio/typescript-tools.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "neovim/nvim-lspconfig",
         },
         config = function()
-            local lspconfig = require("lspconfig")
             require("typescript-tools").setup({
-                root_dir = lspconfig.util.root_pattern("package.json"),
-                single_file_support = false,
+                on_attach = function(_, bufnr)
+                    local keymap_opts = { buffer = bufnr }
+
+                    keymap("n", "gu", "<cmd>TSToolsRemoveUnusedImports<cr>", keymap_opts)
+                    keymap("n", "go", "<cmd>TSToolsSortImports<cr>", keymap_opts)
+                    keymap("n", "gi", "<cmd>TSToolsAddMissingImports<cr>", keymap_opts)
+                end,
             })
         end,
     },
