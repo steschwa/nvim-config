3fd74f02fec7c8fff9fffa55c49ebb8233bf9e5c
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-02-02 15:24:11 +0100

    feat: keymap to open help for word under cursor

--- lua/steschw/utils/keys.lua
+++ lua/steschw/utils/keys.lua
@@ -1,23 +1,32 @@
+--- @class KeymapOpts
+--- @field remap? boolean
+--- @field silent? boolean
+--- @field nowait? boolean
+--- @field buffer? number
+
+--- @type KeymapOpts
 local DEFAULT_OPTS = {
-    noremap = true,
+    remap = false,
     silent = true,
     nowait = true,
 }
 
 local M = {}
 
----@param mode string|string[]
----@param lhs string
----@param rhs string|function
----@param opts? table
+--- @param mode string|string[]
+--- @param lhs string
+--- @param rhs string|function
+--- @param opts? KeymapOpts
 function M.keymap(mode, lhs, rhs, opts)
     local keys = require("lazy.core.handler").handlers.keys
-    --do not create the keymap if a lazy keys handler exists
-    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-        opts = opts or {}
-        opts = vim.tbl_extend("force", DEFAULT_OPTS, opts)
-        vim.keymap.set(mode, lhs, rhs, opts)
+    -- do not create the keymap if a lazy keys handler exists
+    if keys.active[keys.parse({ lhs, mode = mode }).id] then
+        return
     end
+
+    opts = opts or {}
+    opts = vim.tbl_extend("force", DEFAULT_OPTS, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 return M
