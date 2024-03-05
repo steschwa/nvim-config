0a0cd923ca7789820f8e12920766abadccfca3fe
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-03-05 11:34:23 +0100

    feat: init quickfix from clipboard

--- lua/steschw/config/commands.lua
+++ lua/steschw/config/commands.lua
@@ -14,3 +14,42 @@ end, {
     nargs = 1,
     complete = "command",
 })
+
+vim.api.nvim_create_user_command("SetQf", function(opts)
+    local from = opts.fargs[1] or "clipboard"
+
+    local items = {}
+    if from == "clipboard" then
+        local clipboard = vim.fn.getreg("*")
+
+        ---@diagnostic disable-next-line: cast-local-type
+        local ok, clip_items = pcall(vim.json.decode, clipboard)
+        if ok and type(clip_items) == "table" then
+            items = clip_items
+        else
+            return
+        end
+    end
+
+    if #items == 0 then
+        return
+    end
+
+    vim.fn.setqflist(items, " ")
+    vim.cmd("cw")
+end, {
+    force = false,
+    nargs = "?",
+    complete = function(arg_lead)
+        local choices = { "clipboard" }
+
+        local matches = {}
+        for _, choice in ipairs(choices) do
+            if choice:find("^" .. arg_lead) then
+                table.insert(matches, choice)
+            end
+        end
+
+        return matches
+    end,
+})
